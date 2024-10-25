1394049c886efe3d37ad6b6e421df48977cb0236
root <root@holly-pc>
2024-10-25 15:00:53 +0100

    adjusted styling and layout of design page top panel

--- webapp/src/pages/design/TopPanel.tsx
+++ webapp/src/pages/design/TopPanel.tsx
@@ -7,11 +7,14 @@ import {
   MenuItem,
   Button,
   Avatar,
+  Box,
+  Text,
+  IconButton,
 } from '@chakra-ui/react';
-import { FaCog } from 'react-icons/fa';
+import { FileText, Users, Settings, SquareCode, Pencil, PanelsTopLeft } from 'lucide-react';
 import { logEvent } from '../../utils/analytics';
 import InputModal from '../../components/InputModal';
-
+import logo from '../../assets/logo/solai_logo_png.png';
 interface TopPanelProps {
   generatePrompt: () => void;
   onClickInput: () => void;
@@ -27,71 +30,64 @@ const TopPanel: React.FC<TopPanelProps> = ({
   onClickSave,
   onClickNew,
 }) => {
-
   return (
-    <Flex
-      as="header"
-      borderBottom="1px solid"
-      borderColor="gray.200"
-      justifyContent="space-between"
-      alignItems="center"
-      height="14"
-      px={4}
-      shadow="md"
-    >
-      <Flex alignItems="center" gap={6}>
-        <Flex
-          direction="row"
-          alignItems="center"
-          gap={4}
-          ml={4}
-        >
-          <h1 className="text-lg font-semibold">
-            {/* Removed {projectName} */}
-          </h1>
+    <Box as="header" bg="white" borderBottom="1px solid" borderColor="gray.200" px={4} py={3}>
+      <Flex maxW="7xl" mx="auto" alignItems="center" justifyContent="space-between" width="100%">
+        <Flex alignItems="center" gap={4}>
+          <Button as="a" href="/" variant="link" display="flex" alignItems="center" gap={2}>
+            <img src={logo} alt="Logo" className="h-12 w-12" />
+            <Text fontSize="xl" fontWeight="semibold" color="#5688e8">SolAI</Text>
+          </Button>
+        </Flex>
+
+        <Flex as="nav" justifyContent="space-evenly" alignItems="center" gap={1} width="100%">
+          <Flex alignItems="center" gap={1}>
+            <Button variant="ghost" size="sm" colorScheme="blue" onClick={onClickInput}>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit Project Info
+            </Button>
+            <Button variant="ghost" size="sm" colorScheme="gray" onClick={generatePrompt}>
+              <SquareCode className="h-4 w-4 mr-2" />
+              Generate Code
+            </Button>
+          </Flex>
+          <Flex alignItems="center" gap={1}>
+            <Menu>
+              <MenuButton as={Button} variant="ghost" size="sm" colorScheme="gray">
+                <Flex alignItems="center" gap={0}>
+                  <PanelsTopLeft className="h-4 w-4 mr-2" />
+                  Project
+                </Flex>
+              </MenuButton>
+              <MenuList>
+                <MenuItem onClick={onClickOpen}>Open</MenuItem>
+                <MenuItem onClick={onClickSave}>Save</MenuItem>
+                <MenuItem onClick={onClickNew}>New</MenuItem>
+              </MenuList>
+            </Menu>
+            <Button variant="ghost" size="sm" colorScheme="gray">
+              <FileText className="h-4 w-4 mr-2" />
+              Files
+            </Button>
+            <Button variant="ghost" size="sm" colorScheme="gray">
+              <Users className="h-4 w-4 mr-2" />
+              Team
+            </Button>
+          </Flex>
+        </Flex>
+
+        <Flex alignItems="center" gap={4}>
+          <IconButton
+            aria-label="Settings"
+            icon={<Settings className="h-5 w-5" />}
+            variant="ghost"
+            size="sm"
+            colorScheme="gray"
+          />
+          <Avatar size="sm" src="/placeholder.svg" />
         </Flex>
       </Flex>
-      <Flex>
-        <Button onClick={onClickInput}>Input</Button>
-        <Menu>
-          <MenuButton as={Button} variant="ghost" size="sm">
-            Project
-          </MenuButton>
-          <MenuList>
-            <MenuItem onClick={onClickOpen}>Open</MenuItem>
-            <MenuItem onClick={onClickSave}>Save</MenuItem>
-            <MenuItem onClick={onClickNew}>New</MenuItem>
-          </MenuList>
-        </Menu>
-        <Menu>
-          <MenuButton as={Button} variant="ghost" size="sm">
-            Team
-          </MenuButton>
-          <MenuList>
-            <MenuItem onClick={() => console.log('Manage')} isDisabled={true}>
-              Manage
-            </MenuItem>
-            <MenuItem onClick={() => console.log('Invite')} isDisabled={true}>
-              Invite
-            </MenuItem>
-          </MenuList>
-        </Menu>
-        <Button
-          leftIcon={React.createElement(FaCog)}
-          variant="ghost"
-          size="sm"
-          onClick={generatePrompt}
-        >
-          Prompt
-        </Button>
-      </Flex>
-      <Flex alignItems="center" gap={4}>
-        <Button variant="ghost" size="sm">
-          <FaCog className="h-5 w-5" />
-        </Button>
-        <Avatar size="sm" src="/placeholder.svg" />
-      </Flex>
-    </Flex>
+    </Box>
   );
 };
 
