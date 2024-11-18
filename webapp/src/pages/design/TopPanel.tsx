4a954ec521e3bd7c3fed27c96272e9ff2d79cf4e
root <root@holly-pc>
2024-11-18 11:45:20 +0000

    style changes to design page top panel

--- webapp/src/pages/design/TopPanel.tsx
+++ webapp/src/pages/design/TopPanel.tsx
@@ -5,15 +5,19 @@ import {
   Avatar,
   Box,
   Text,
-  useToast,
   Menu,
   MenuButton,
   MenuList,
   MenuItem,
+  IconButton,
+  Divider,
+  Tooltip,
+  Input,
 } from '@chakra-ui/react';
-import { Pencil, WalletIcon, Check } from 'lucide-react';
+import { ChevronDownIcon, EditIcon } from '@chakra-ui/icons';
+import { Check, FolderOpen, Wallet, Plus, Code, Save, Sparkles } from 'lucide-react';
+import { HiOutlineSparkles } from "react-icons/hi";
 import { useProjectContext } from '../../contexts/ProjectContext';
-import { promptAI } from '../../services/prompt';
 
 interface TopPanelProps {
   generatePrompt: () => void;
@@ -42,97 +46,101 @@ const TopPanel: React.FC<TopPanelProps> = ({
   const [apiKey, setApiKey] = useState('');
 
   const handleMouseEnter = () => setHover(true);
-  const handleMouseLeave = () => setHover(false); 
+  const handleMouseLeave = () => setHover(false);
 
   useEffect(() => {
     console.log('apiKey:', apiKey);
   }, [apiKey]);
 
   return (
-    <Flex as="header" bg="white" borderBottom="1px solid" borderColor="gray.200" p={1} justifyContent="space-evenly" alignItems="center">
-      <Flex flex={1} alignItems="center" justifyContent="space-evenly" px={8} gap={10}>
-        <Flex alignItems="center" gap={2}>
-          <Button variant="ghost" size="xs" colorScheme="gray" onClick={onClickOpen}>Open</Button>
-          <Button variant="ghost" size="xs" colorScheme="gray" onClick={onClickSave}>Save</Button>
-          <Button variant="ghost" size="xs" colorScheme="gray" onClick={onClickNew}>New</Button>
-          <Menu>
-            <MenuButton as={Button} variant="ghost" size="xs" colorScheme="blue">
-              <Text fontSize="xs" color="#5688e8" fontWeight="medium">Select AI Model</Text>
-            </MenuButton>
-            <MenuList>
-              <MenuItem onClick={() => {onSelectModel('codestral-latest', ''); setSelectedModel('codestral-latest')}}><Text fontSize="xs">codestral-latest</Text></MenuItem>
-              <MenuItem onClick={() => {onSelectModel('gpt-4o', apiKey); setSelectedModel('gpt-4o')}}><Text fontSize="xs">gpt-4o</Text></MenuItem>
-            </MenuList>
-          </Menu>
-          {selectedModel === 'gpt-4o' && (
-            <Box>
-              <input
-                type="text"
-                placeholder="Enter API Key"
-                value={apiKey}
-                onChange={(e) => {
-                  setApiKey(e.target.value);
-                }}
-                style={{ marginLeft: '10px', padding: '2px', fontSize: '12px' }}
-              />
+    <Flex as="header" h="14" align="center" gap={4} borderBottomWidth="1px" bg="gray.50" px={6}>
+      <Flex align="center" gap={4}>
+        <Tooltip label="Open Project" mt={2} bg="gray.100" size="xs" shadow="md" color="gray.700" fontSize="xs" fontWeight="normal">
+          <IconButton 
+            aria-label="Open" 
+            icon={<FolderOpen size={14} />} 
+            variant="ghost" 
+            size="1rem" 
+            onClick={onClickOpen} 
+          />
+        </Tooltip>
+        <Tooltip label="Save Project" mt={2} bg="gray.100" size="xs" shadow="md" color="gray.700" fontSize="xs" fontWeight="normal">
+          <IconButton 
+            aria-label="Save" 
+            icon={<Save size={14} />} 
+            variant="ghost" 
+            size="1rem" 
+            onClick={onClickSave} 
+          />
+        </Tooltip>
+        <Tooltip label="New Project" mt={2} bg="gray.100" size="xs" shadow="md" color="gray.700" fontSize="xs" fontWeight="normal">
+          <IconButton 
+            aria-label="New" 
+            icon={<Plus size={14} />} 
+            variant="ghost" 
+            size="1rem" 
+            onClick={onClickNew} 
+          />
+        </Tooltip>
+      </Flex>
+      <Divider orientation="vertical" />
+      <Flex align="center" gap={4}>
+        <Menu>
+          <MenuButton p={3} as={Button} leftIcon={<HiOutlineSparkles size={14} />} rightIcon={<ChevronDownIcon />} variant="outline" size="xs">
+            <Text fontSize="xs">Select AI Model</Text>
+          </MenuButton>
+          <MenuList>
+            <MenuItem onClick={() => { onSelectModel('codestral-latest', ''); setSelectedModel('codestral-latest'); }}><Text fontSize="xs">codestral-latest</Text></MenuItem>
+            <MenuItem onClick={() => { onSelectModel('gpt-4o', apiKey); setSelectedModel('gpt-4o'); }}><Text fontSize="xs">gpt-4o</Text></MenuItem>
+          </MenuList>
+        </Menu>
+        {selectedModel === 'gpt-4o' && (
+          <Flex align="center" gap={1}>
+            <Input
+              placeholder="Enter API Key"
+              value={apiKey}
+              onChange={(e) => setApiKey(e.target.value)}
+              size="xs"
+              width="200px"
+            />
+            <Tooltip label="Save API Key" mt={2} bg="gray.100" size="xs" shadow="md" color="gray.700" fontSize="xs" fontWeight="normal">
               <Button
                 variant="ghost"
                 size="xs"
                 colorScheme="blue"
                 onClick={() => {
                   setProjectContext({ ...projectContext, apiKey });
-                }}
-                style={{ marginLeft: '5px' }}
-                leftIcon={<Check size={16} />}
+              }}
+              leftIcon={<Check size={14} />}
               >
               </Button>
-            </Box>
-          )}
-          <Button variant="ghost" size="xs" colorScheme="blue" onClick={generatePrompt}>
-            <Text fontSize="xs" color="#5688e8" fontWeight="medium">Generate Code</Text>
-          </Button>
-        </Flex>
-        <Flex justifyContent="space-between" alignItems="center" gap={2} flexGrow={1}>
-          <Flex flex={1} alignItems="center" justifyContent="space-evenly" gap={8}>
-            <Box
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
-              p={2}
-              gap={2}
-              borderRadius="md"
-              position="relative"
-              fontSize="xs"
-              fontWeight="medium"
-              color="gray.700"
-            >
-              {projectContext.name || 'Untitled Project'}
-              {hover && (
-                <Box
-                  bg="gray.100"
-                  p={2}
-                  borderRadius="md"
-                  position="absolute"
-                  top="100%"
-                  mt={2}
-                  width="250px"
-                  maxWidth="300px"
-                  zIndex="tooltip"
-                  boxShadow="md"
-                >
-                  {projectContext.description && <Text fontSize="xs">{projectContext.description}</Text>}
-                </Box>
-              )}
-              <Button variant="ghost" size="xs" colorScheme="gray" onClick={onClickInput}><Pencil className="h-3 w-3" /></Button>
-            </Box>
+            </Tooltip>
           </Flex>
-        </Flex>
-        <Button variant="ghost" size="xs" colorScheme="blue" onClick={onToggleWallet} leftIcon={<WalletIcon size={16} />}>Wallet</Button>
-        <Flex alignItems="center" gap={1} padding={1}>
-          <Flex alignItems="center" gap={4}>
+        )}
+        <Button leftIcon={<Code size={12} />} onClick={generatePrompt} size="xs" p={3}>
+          Generate Code
+        </Button>
+      </Flex>
+      <Flex flex="1" justify="center" align="center">
+        <Button variant="ghost" size="xs" onClick={onClickInput}>
+          {projectContext.name || 'Untitled Project'}
+          <EditIcon ml={2} />
+        </Button>
+      </Flex>
+      <Flex align="center" gap={4}>
+        <Button leftIcon={<Wallet size={12} />} variant="outline" size="xs" onClick={onToggleWallet}>
+          <Text fontSize="xs">Wallet</Text>
+        </Button>
+        <Menu>
+          <MenuButton as={Button} variant="ghost" size="xs" rounded="full">
             <Avatar size="xs" src="/placeholder.svg" />
-            <Button variant="ghost" size="xs" colorScheme="red" onClick={onLogout}>Logout</Button>
-          </Flex>
-        </Flex>
+          </MenuButton>
+          <MenuList w="auto">
+            <MenuItem onClick={onLogout} p={0} pl={2}>
+              <Text fontSize="xs">Logout</Text>
+            </MenuItem>
+          </MenuList>
+        </Menu>
       </Flex>
     </Flex>
   );
